68c212ca404ac3bceca377e08a37e8ce95b4739f
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-04-12 15:52:45 +0100

    Sorted into folders

--- src/main/java/io/itch/awesomekalin/noob/itemgroup/NoobTabItemGroup.java
+++ src/main/java/io/itch/awesomekalin/noob/itemgroup/NoobTabItemGroup.java
@@ -6,23 +6,23 @@ import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.block.Blocks;
 
-import io.itch.awesomekalin.noob.block.NoobChestBlock;
 import io.itch.awesomekalin.noob.NoobModElements;
 
 @NoobModElements.ModElement.Tag
 public class NoobTabItemGroup extends NoobModElements.ModElement {
 	public NoobTabItemGroup(NoobModElements instance) {
-		super(instance, 4);
+		super(instance, 58);
 	}
 
 	@Override
 	public void initElements() {
-		tab = new ItemGroup("tabnoobtab") {
+		tab = new ItemGroup("tabnoob_tab") {
 			@OnlyIn(Dist.CLIENT)
 			@Override
 			public ItemStack createIcon() {
-				return new ItemStack(NoobChestBlock.block, (int) (1));
+				return new ItemStack(Blocks.DIRT, (int) (1));
 			}
 
 			@OnlyIn(Dist.CLIENT)
